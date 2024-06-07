f26eae9aa32ab8eea7f46d1b90ecc1588ee5e2a5
Sandhiya Acharya <43362245+SandhiyaAcharya@users.noreply.github.com>
2024-06-08 00:20:56 +0530

    Add files via upload
    Updated model.

--- 6. Project Executable Files/flask/app.py
+++ 6. Project Executable Files/flask/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request 
-import pickle
+import pickle, joblib 
 import pandas as pd
-import numpy as np
 
 app = Flask(__name__)
 
+
 model = pickle.load(open("model.pkl",'rb'))
-# ct = joblib.load('feature_values.pkl')
+ct = joblib.load('feature_values')
 
 @app.route('/')
 def home():
@@ -18,36 +18,54 @@ def predict():
 
 @app.route('/out', methods =["POST"])
 def output():
-    age = request.form.get("age")
-    gender = request.form.get("gender",)
-    self_employed = request.form.get("self_employed")
-    family_history= request.form.get("family_history")
-    work_interfere = request.form.get("work_interfere")
-    no_employees = request.form.get("no_employees")
-    remote_work = request.form.get("remote_work")
-    tech_company =request.form.get("tech_company")
-    benefits = request.form.get("benefits")
-    care_options = request.form.get("care_options")
-    wellness_program = request.form.get("wellness_program")
-    seek_help = request.form.get("seek_help")
-    anonymity = request.form.get("anonymity")
-    leave = request.form.get("leave")
-    mental_health_consequence = request.form.get("mental_health_consequence")
-    phys_health_consequence = request.form.get("phys_health_consequence")
-    coworkers = request.form.get("coworkers")
-    supervisor = request.form.get("supervisor")
-    mental_health_interview = request.form.get("mental_health_interview")
-    phys_health_interview = request.form.get("phys_health_interview")
-    mental_vs_physical = request.form.get("mental_vs_physical")
-    obs_consequence = request.form.get("obs_consequence")
-    
-    df = pd.DataFrame({'age': [age], 'gender': [gender], 'self_employed':[self_employed], 'family_history':[family_history], 'work_interfere':[work_interfere], 'no_employees':[no_employees], 'remote_work':[remote_work], 'tech_company':[tech_company], 'benefits':[benefits], 'care_options':[care_options], 'wellness_program':[wellness_program], 'seek_help':[seek_help], 'anonymity':[anonymity], 'leave':[leave], 'mental_health_consequence':[mental_health_consequence], 'phys_health_consequence':[phys_health_consequence], 'coworkers':[coworkers], 'supervisor':[supervisor], 'mental_health_interview':[mental_health_interview], 'phys_health_interview':[phys_health_interview], 'mental_vs_physical':[mental_vs_physical], 'obs_consequence':[obs_consequence]})
-    
-    prediction = model.predict(df)  # predict using the DataFrame
-    
-    output = "the prediction is " + str(['yes', 'no'][np.argmax(pred)])
-    
-    return render_template(output, "output.html")
+    age = request.form["age"]
+    gender = request.form["gender"]
+    self_employed = request.form["self_employed"]
+    family_history= request.form["family_history"]
+    work_interfere = request.form["work_interfere"]
+    no_employees = request.form["no_employees"]
+    remote_work = request.form["remote_work"]
+    tech_company =request.form["tech_company"]
+    benefits = request.form["benefits"]
+    care_options = request.form["care_options"]
+    wellness_program = request.form["wellness_program"]
+    seek_help = request.form["seek_help"]
+    anonymity = request.form["anonymity"]
+    leave = request.form["Leave"]
+    mental_health_consequence = request.form["mental_health_consequence"]
+    phys_health_consequence = request.form["phys_health_consequence"]
+    coworkers = request.form["coworkers"]
+    supervisor = request.form["supervisor"]
+    mental_health_interview = request.form["mental_health_interview"]
+    phys_health_interview = request.form["phys_health_interview"]
+    mental_vs_physical = request.form["mental_vs_physical"]
+    obs_consequence = request.form["obs_consequence"]
+
+    data = [[age, gender, self_employed, family_history, work_interfere, no_employees, remote_work, tech_company, benefits, care_options, wellness_program, seek_help, anonymity, leave, mental_health_consequence, phys_health_consequence, coworkers, supervisor, mental_health_interview, phys_health_interview, mental_vs_physical, obs_consequence]]
+
+    feature_cols = ['Age', 'Gender', 'self_employed', 'family_history',
+
+    'work_interfere', 'no_employees', 'remote_work', 'tech_company',
+
+    'benefits', 'care_options', 'wellness_program', 'seek_help',
+
+    'anonymity', 'Leave', 'mental_health_consequence',
+
+    'phys_health_consequence', 'coworkers', 'supervisor',
+
+    'mental_health_interview', 'phys_health_interview',
+
+    'mental_vs_physical', 'obs_consequence']
+
+    pred =model.predict(ct.transform(pd.DataFrame (data, columns=feature_cols)))
+
+    pred = pred[0]
+
+    if pred:
+        return render_template("output.html",y="This person requires mental health treatment ")
+    else:
+        return render_template("output.html",y="This person doesn't require mental health treatment")
+
 
 if __name__ == '_main_':
-    app.run(debug = False)
+    app.run(debug = True)
